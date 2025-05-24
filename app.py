0a87c38d09e0c63ee5a43b631470b3cb75dfe57d
Vikneshoftheleaf <114404122+Vikneshoftheleaf@users.noreply.github.com>
2025-05-24 16:25:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def generate():
 
         # Generate chat completion
         chat_completion = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="mistral-saba-24b",
             messages=messages,
             
         )
